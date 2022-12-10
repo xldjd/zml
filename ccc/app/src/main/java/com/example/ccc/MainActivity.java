e889dd4e4d7d67eb3b5690b344afd4ab3db3f74d
xl <1485384932@qq.com>
2022-12-10 19:07:37 +0800

    kl

---
+++ ccc/app/src/main/java/com/example/ccc/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.ccc;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        }
+
+    @Override
+    public void onClick(View view) {
+
+}}
